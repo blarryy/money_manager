b350597160145c7aedbe448f35a551ae2522a420
blarryy <34322953+blarryy@users.noreply.github.com>
2018-06-12 10:28:14 -0700

    serverside auth

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import Header from '../components/Header';
-export const PrivateRoute = ({ 
+
+export const PublicRoute = ({ 
     isAuthenticated,  //equals to isAuthenticated = props.isAuthticated.
     component: Component, // equals to component = props.component, and then
     ...rest                 // give component a new name: Component
@@ -10,11 +10,9 @@ export const PrivateRoute = ({
     return (
     <Route {...rest} component={(props) => (
         isAuthenticated ? (
-            <div>
-            <Header />
-            <Component {...props} /></div>
+            <Redirect to="/dashboard" />
         ) : (
-            <Redirect to="/" />
+            <Component {...props} />
         )
     )}/>
 );
@@ -24,5 +22,4 @@ const mapStateToProps = (state) => ({
     isAuthenticated: !!state.auth.uid
 });
 
-export default connect(mapStateToProps)(PrivateRoute);
-
+export default connect(mapStateToProps)(PublicRoute);
